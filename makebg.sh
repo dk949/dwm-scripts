f89a22bd992e2f5111904c64b9bb1d5d3e682947
dk949 <56653556+dk949@users.noreply.github.com>
2023-04-26 02:00:09 +0100

    fixed makebg

--- makebg.sh
+++ makebg.sh
@@ -4,7 +4,7 @@ if test "$#" -ne 1; then
     exit 1
 fi
 
-CACHE_FILE_NAME=fehbg
+CACHE_FILE_NAME=.fehbg
 
 if ! feh --bg-fill "$1" && mv "$HOME/.$CACHE_FILE_NAME" "$XDG_CACHE_HOME/$CACHE_FILE_NAME"; then
     echo "makebg failed for unknown reason"
